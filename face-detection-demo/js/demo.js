17cf08cd1c5202c1af298af9ab8e13084bb158cd
Rich Rein <richard.rein@pearson.com>
2016-01-27 11:30:02 -0600

    Align with WebRTC
    updated demo js to match WebRTC example

--- face-detection-demo/js/demo.js
+++ face-detection-demo/js/demo.js
@@ -7,9 +7,37 @@
  (function () {
 	'use strict';
 
+	// Put variables in global scope to make them available to the browser console.
 	var video = document.querySelector('video');
+	var canvas = window.canvas = document.querySelector('canvas');
+	canvas.width = 480;
+	canvas.height = 360;
 
-	var App = {
+	var button = document.querySelector('button');
+	button.onclick = function() {
+	  canvas.width = video.videoWidth;
+	  canvas.height = video.videoHeight;
+	  canvas.getContext('2d').
+	    drawImage(video, 0, 0, canvas.width, canvas.height);
+	};
+
+	var constraints = {
+	  audio: false,
+	  video: true
+	};
+
+	function successCallback(stream) {
+	  window.stream = stream; // make stream available to browser console
+	  video.srcObject = stream;
+	}
+
+	function errorCallback(error) {
+	  console.log('navigator.getUserMedia error: ', error);
+	}
+
+	navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+	/*var App = {
 
 		init: function () {
 
@@ -121,7 +149,7 @@
 
 			if (App.options.context === 'webrtc') {
 
-				/*var video = App.options.videoEl;
+				var video = App.options.videoEl;
 				
 				if ((typeof MediaStream !== "undefined" && MediaStream !== null) && stream instanceof MediaStream) {
 		
@@ -141,10 +169,7 @@
 				video.onerror = function () {
 					stream.stop();
 					streamError();
-				};*/
-
-				window.stream = stream; // make stream available to browser console
-  				video.srcObject = stream;
+				};
 			} else{
 				// flash context
 			}
@@ -257,7 +282,7 @@
 
 	};
 
-	App.init();
+	App.init();*/
 
 })();
 
